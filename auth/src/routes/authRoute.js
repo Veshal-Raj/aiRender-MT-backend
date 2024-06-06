be5a437922db3b3fe0a09473a5056dc1a31f310c
Veshal Raj <veshalraj1307@gmail.com>
2024-06-06 23:51:35 +0530

    refactor: updated the folder structure

--- auth/src/routes/authRoute.js
+++ auth/src/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginController, signupController } from "../controllers/authController";
+import { loginController, signupController } from "../../controllers/authController";
 
 const router = express.Router()
 
